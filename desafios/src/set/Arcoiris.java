c9cdf787ab907ca184dea84fd2c1b83c38d26cc1
Laiz Carvalho Zanlucas <zanlucaslaiz@gmail.com>
2022-12-21 20:55:21 -0300

    novos desafios

---
+++ desafios/src/set/Arcoiris.java
@@ -0,0 +1,12 @@
+package set;
+// Crie um conjunto contendo as cores do arco-íris e:
+// Exiba todas as cores uma abaixo da outra
+// A quantidade de cores que o arco-íris tem
+// Exiba as cores em ordem alfabética
+// Exiba as cores na ordem inversa da que foi informada
+// Exiba todas as cores que começam com a letra ”v”
+// Remova todas as cores que não começam com a letra “v”
+// Limpe o conjunto
+// Confira se o conjunto está vazio
+
+// usando o set
