c0632c9dade9b3ac4baf8d164f1318ace7921009
Laiz Carvalho Zanlucas <zanlucaslaiz@gmail.com>
2023-01-04 09:18:15 -0300

    desafios concluidos

--- desafios/src/DiferencaDigitos.java
+++ desafios/src/DiferencaDigitos.java
@@ -1,20 +1,21 @@
-// Desafio
-// O desafio consiste em um número inteiro n, onde será necessário calcular a diferença entre o produto e a soma dos seus digitos.
+/*Desafio
+O desafio consiste em um número inteiro n, onde será necessário calcular a diferença entre o produto e a 
+soma dos seus digitos.
 
-// Entrada 
-// • Para encontrar o produto do valor, os dígitos deverão ser multiplicados.
+Entrada 
+• Para encontrar o produto do valor, os dígitos deverão ser multiplicados.
 
-// • A soma, basta realizar uma adição entre os dígitos.
+• A soma, basta realizar uma adição entre os dígitos.
 
-// • A diferença, é necessário substrair o resultado final dos dois valores.
+• A diferença, é necessário substrair o resultado final dos dois valores.
 
-// Saída
-// A saída deverá retornar o valor final da diferença entre o produto e a soma dos digitos.
+Saída
+A saída deverá retornar o valor final da diferença entre o produto e a soma dos digitos.
 
-// Explicação:
-// Produto de dígitos = 2 * 3 * 4 = 24
-// Soma dos dígitos = 2 + 3 + 4 = 9
-// Resultado = 24 - 9 = 15
+Explicação:
+Produto de dígitos = 2 * 3 * 4 = 24
+Soma dos dígitos = 2 + 3 + 4 = 9
+Resultado = 24 - 9 = 15 */
 
 import java.util.Arrays;
 import java.util.Scanner;
@@ -25,10 +26,21 @@ public class DiferencaDigitos {
 
    int numero = scan.nextInt();
 
+   int produto = 1;
+   int soma = 0;
+
    String numeroEmString = String.valueOf(numero);
    String[] split = numeroEmString.split("");
 
    int[] ints = Arrays.stream(split).mapToInt(Integer::new).toArray();
+
+   for(int x=0; x<ints.length; x++) {
+    produto *= ints[x];
+    soma += ints[x];
+
+   }
+
+   System.out.println(produto - soma);
 
        // TODO: Retorne a diferença entre o produto e a soma dos dígitos.
        
--- desafios/src/Emboscada.java
+++ desafios/src/Emboscada.java
@@ -1,27 +1,37 @@
-// Desafio
-// Você e seus amigos estão jogando uma partida de RPG (Role-Playing Game) utilizando um dado de 10 lados. Em um determinado momento, em uma masmorra escura, o seu personagem foi emboscado por um terrível goblin. Como em todo bom RPG, temos alguns parâmetros que definem os status dos personagens:
-// Vida: Pontos que devem ser maiores que 0, se não o personagem morre.
-// Ataque: Pontos que definem quanto de dano ele inflingirá nos outros personagens.
-// Def/esa: Pontos que definem quanto dano será mitigado quando ele levar um ataque.
-// O dado é um modificador desses parâmetros. Quando você joga o dado o valor dele deve ser somado ao seu ataque ou a sua defesa. Quando o valor do dado for maior que 8 e menor que 10, o jogador tira um crítico e essa soma deve ser DOBRADA.
-// O dano causado ao jogador é calculado dessa forma:
-// vidaDoJogador - (ataqueInimigo - (defesaDoJogador + dados))
-// O dano causado ao inimigo é calculado dessa forma:
-// vidaDoInimigo - (ataqueDoJogador + dados)
-// Caso o jogador sobreviva a emboscada, ele contra-ataca.
-// Para deixar a jogatina mais rápida, fazendo o computador realizar as contas matemáticas, você decidiu criar esse programa para verificar se seu personagem vai sobreviver e contra-atacar ou morrer.
+/* Desafio
+Você e seus amigos estão jogando uma partida de RPG (Role-Playing Game) utilizando um dado de 10 lados. 
+Em um determinado momento, em uma masmorra escura, o seu personagem foi emboscado por um terrível goblin. 
+Como em todo bom RPG, temos alguns parâmetros que definem os status dos personagens:
+- Vida: Pontos que devem ser maiores que 0, se não o personagem morre.
+- Ataque: Pontos que definem quanto de dano ele inflingirá nos outros personagens.
+- Defesa: Pontos que definem quanto dano será mitigado quando ele levar um ataque.
 
-// Entrada
-// A entrada serão seis parâmetros nessa ordem:
-// Dados;
-// Vida do Jogador;
-// Ataque do Jogador;
-// Defesa do Jogador
-// Vida do Inimigo;
-// Ataque do Inimigo;
+O dado é um modificador desses parâmetros. Quando você joga o dado o valor dele deve ser somado ao seu ataque 
+ou a sua defesa. Quando o valor do dado for maior que 8 e menor que 10, o jogador tira um crítico 
+e essa soma deve ser DOBRADA.
 
-// Saída
-// A saída deve ser verificado se o jogador sobreviveu ao ataque ou não. Caso ele tenha sobrevivido, verificar se o contra-ataque dele foi eficaz e matou o inimigo ou não. Em seguida, para cada caso, imprima na tela as respectivas ocorrências.
+O dano causado ao jogador é calculado dessa forma:
+vidaDoJogador - (ataqueInimigo - (defesaDoJogador + dados))
+O dano causado ao inimigo é calculado dessa forma:
+vidaDoInimigo - (ataqueDoJogador + dados)
+Caso o jogador sobreviva a emboscada, ele contra-ataca.
+Para deixar a jogatina mais rápida, fazendo o computador realizar as contas matemáticas, você decidiu criar esse 
+programa para verificar se seu personagem vai sobreviver e contra-atacar ou morrer.
+
+Entrada
+
+A entrada serão seis parâmetros nessa ordem:
+- Dados;
+- Vida do Jogador;
+- Ataque do Jogador;
+- Defesa do Jogador
+- Vida do Inimigo;
+- Ataque do Inimigo;
+
+Saída
+A saída deve ser verificado se o jogador sobreviveu ao ataque ou não. Caso ele tenha sobrevivido, verificar se 
+o contra-ataque dele foi eficaz e matou o inimigo ou não. Em seguida, para cada caso, imprima na tela as respectivas 
+ocorrências. */
 
 
 import java.util.Scanner; 
@@ -37,6 +47,25 @@ public class Emboscada {
         int vidaDoInimigo= leitor.nextInt(); 
         int ataqueDoInimigo = leitor.nextInt();
 
-     //TODO: Crie as condições necessárias para verificar se o jogador vai sobreviver, e contra-atacar, ou morrer;
-     }
+        if(dados == 9){
+            vidaDoJogador = vidaDoJogador + dados * 2;
+            defesaDoJogador = defesaDoJogador + dados * 2;
+        }       
+        
+        vidaDoJogador = vidaDoJogador - (ataqueDoInimigo - (defesaDoJogador + dados));
+        if(vidaDoJogador > 0){
+            vidaDoInimigo = vidaDoInimigo - (ataqueDoJogador + dados);
+            if(vidaDoInimigo > 0){
+                System.out.println( "Jogador sobreviveu e nao derrotou o inimigo");
+            }
+            else{      
+                System.out.println( "Jogador sobreviveu e derrotou o inimigo");
+            }
+        }
+        else{      
+           System.out.println( "Jogador nao sobreviveu");
+        }
+       
+        leitor.close();     
+    }
 }
